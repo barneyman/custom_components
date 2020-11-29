aa47af69815dffedd8df454d6f2a9ab941c04a98
barneyman <barnaby.james.flint@gmail.com>
2020-11-29 18:04:01 +1100

    multiple luxlights - phase 1

--- luxlights.py
+++ luxlights.py
@@ -20,138 +20,122 @@ import homeassistant.helpers.config_validation as cv
 
 
 # Validation of the user's configuration
+
+# instanceSchema=vol.Schema(
+#         {
+#             vol.Required("presence_binary"): cv.string,  
+#             vol.Required("lux_sensor"): cv.string,
+#             vol.Required("off_scene"): cv.string,
+#             vol.Optional("jitter", default=10):vol.All(
+#                     vol.Coerce(float), vol.Range(min=0, max=20)
+#                 ),
+#             "absent": {
+#                 vol.Optional("unlit_scene"): cv.string,
+#                 vol.Optional("lit_scene"): cv.string,
+#                 vol.Required("minLux"): vol.All(
+#                     vol.Coerce(float), vol.Range(min=0, max=100)
+#                 ),
+#             },
+#             "present": {
+#                 vol.Optional("unlit_scene"): cv.string,
+#                 vol.Optional("lit_scene"): cv.string,
+#                 vol.Required("minLux"): vol.All(
+#                     vol.Coerce(float), vol.Range(min=0, max=100)
+#                 ),
+#             }
+#         }
+#     )
+
+
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
-                vol.Required("presence_binary"): cv.string,  # sensor.headcount_sensor
-                vol.Required("lux_sensor"): cv.string,
-                vol.Required("off_scene"): cv.string,
-                vol.Optional("jitter", default=10):vol.All(
-                        vol.Coerce(float), vol.Range(min=0, max=20)
-                    ),
-                "absent": {
-                    vol.Optional("unlit_scene"): cv.string,
-                    vol.Optional("lit_scene"): cv.string,
-                    vol.Required("minLux"): vol.All(
-                        vol.Coerce(float), vol.Range(min=0, max=100)
-                    ),
-                },
-                "present": {
-                    vol.Optional("unlit_scene"): cv.string,
-                    vol.Optional("lit_scene"): cv.string,
-                    vol.Required("minLux"): vol.All(
-                        vol.Coerce(float), vol.Range(min=0, max=100)
-                    ),
-                },
+                "city": vol.Schema({
+                    vol.Required("presence_binary"): cv.string,  
+                    vol.Required("lux_sensor"): cv.string,
+                    vol.Required("off_scene"): cv.string,
+                    vol.Optional("jitter", default=10):vol.All(
+                            vol.Coerce(float), vol.Range(min=0, max=20)
+                        ),
+                    "absent": vol.Schema({
+                        vol.Optional("unlit_scene"): cv.string,
+                        vol.Optional("lit_scene"): cv.string,
+                        vol.Required("minLux"): vol.All(
+                            vol.Coerce(float), vol.Range(min=0, max=100)
+                        ),
+                    }),
+                    "present": vol.Schema({
+                        vol.Optional("unlit_scene"): cv.string,
+                        vol.Optional("lit_scene"): cv.string,
+                        vol.Required("minLux"): vol.All(
+                            vol.Coerce(float), vol.Range(min=0, max=100)
+                        ),
+                    }),
+                })
             }
         )
-    },
-    extra=vol.ALLOW_EXTRA,
+    }, extra=vol.ALLOW_EXTRA
 )
 
+luxInstances=[]
 
-def setup(hass, baseConfig):
-    """Set up is called when Home Assistant is loading our component."""
 
-    _LOGGER.info("luxlights setup")
+class luxLightInstance:
 
-    config = baseConfig[DOMAIN]
+    def __init__(self, hass, config, name):
 
-    _HEADCOUNT_BINARY = config.get("presence_binary")
-    _LUX_SENSOR = config.get("lux_sensor")
+        self._hass=hass
+        self._name=name
+        self._config=config
 
-    _ABSENT = config["absent"]
-    _PRESENT = config["present"]
+        self._headcountSensor=config.get("presence_binary")
+        self._luxSensor=config.get("lux_sensor")
+        self._luxJitter=config.get("jitter")
+        self._offScene=config.get("off_scene")
 
-    _JITTER=config.get("jitter")
+        self._absentDetail=config["absent"]
+        self._presentDetail=config["present"]
 
-    _SCENE_OFF = config.get("off_scene")
+        # prime the storing
+        hass.data[DOMAIN] = { name: {} }
 
-    # the ln(lux) below which we trigger ON
-    # occupied, we open the blind
-    ## _MIN_LUX_VALUE_OCCUPIED = config.get("minlux_Occupied")
+        lastKnownRunState = {"state": "unknown", "scene": "unknown"}
+        self.saveState(lastKnownRunState)
 
-    # uoccupied, we don't
-    ## _MIN_LUX_VALUE = config.get("minlux_Vacant")
+        async def _async_sensor_changed(entity_id, old_state, new_state):
+            """Handle temperature changes."""
+            if new_state is None:
+                return
 
-    ## _SCENE_VACANT = config.get("scene_vacant")
-    ## _SCENE_OCCUPIED = config.get("scene_occupied")
-    ## _SCENE_FAKE = config.get("scene_fake")
+            _LOGGER.info("State change for {} forcing lux check for {}".format(entity_id, name))
 
-    def saveState(data):
-        hass.data[DOMAIN] = json.dumps(data)
+            result = await hass.async_add_executor_job(self.handleSensoryCheck)
 
-    #        with open(mypersistfile, 'w') as outfile:
-    #            json.dump(data, outfile)
 
-    lastKnownRunState = {"state": "unknown", "scene": "unknown"}
-    saveState(lastKnownRunState)
+        async_track_state_change(hass, self._headcountSensor, _async_sensor_changed)
+        async_track_state_change(hass, self._luxSensor, _async_sensor_changed)
 
-    def loadState():
-        #        with open(mypersistfile) as json_file:
-        #            return json.load(json_file)
-        return json.loads(hass.data[DOMAIN])
 
-    # TODO check this function
-    def handle_turnoff(call):
-
-        home = (hass.states.get(_HEADCOUNT_BINARY).state)
-
-        if home=="on":
-            # turn on occupied
-            _LOGGER.info("off ignored")
-        else:
-            service_data = {"entity_id": _SCENE_OFF}
-            _LOGGER.info("to vacant")
-            lastKnownRunState = {"state": "off", "scene": _SCENE_OFF}
-
-            hass.services.call("scene", "turn_on", service_data)
-            saveState(lastKnownRunState)
-            # tell the boss
-            # hass.services.call(
-            #     "hangouts",
-            #     "send_message",
-            #     {
-            #         "target": [{"id": "UgyKl3VkJ2F2G_ykyb14AaABAagBjd2cDg"}],
-            #         "message": [{"text": "Lights OFF at the beach house"}],
-            #     },
-            # )
-
-    # this gets called from an automation, it's a hard OFF
-    def handle_forceoff(call):
-
-        service_data = {"entity_id": _SCENE_OFF}
-        _LOGGER.info("to vacant FORCED")
-        lastKnownRunState = {"state": "off", "scene": _SCENE_OFF}
-        hass.services.call("scene", "turn_on", service_data)
-        saveState(lastKnownRunState)
-
-    # reset my state, normally done sunset-hrs
-    def handle_enableCheck(call):
-
-        lastKnownRunState = {"state": "check", "scene": "unknown"}
-        saveState(lastKnownRunState)
-
-    def handle_check():
+    def handleSensoryCheck(self):
 
         # this should run every five mins from before sunset to 11pm
         # first, check the lux
-        _LOGGER.info("reading sensors")
-        lux = hass.states.get(_LUX_SENSOR)
-        hcountBinary = hass.states.get(_HEADCOUNT_BINARY)
+        _LOGGER.info("reading sensors {} {} for instance '{}'".format(self._luxSensor,self._headcountSensor,self._name))
+        lux = self._hass.states.get(self._luxSensor)
+        hcountBinary = self._hass.states.get(self._headcountSensor)
         # sensor.lux_sensor
         _LOGGER.debug("Lux sensor %s", str(lux))
         _LOGGER.debug("hcountBinary sensor %s", str(hcountBinary))
 
         # only run if we're enabled (happens by dusk event in automation) after we're turned off
         # also allows for 'unknown' after a reboot
-        lastKnownRunState = loadState()
+        self._lastKnownRunState = self.loadState()
 
         _LOGGER.debug("lastKnownRunState")
-        _LOGGER.debug(lastKnownRunState)
+        _LOGGER.debug(self._lastKnownRunState)
 
-        if lastKnownRunState["state"] == "off":
+        if self._lastKnownRunState["state"] == "off":
             return
 
         home = 0
@@ -163,8 +147,12 @@ def setup(hass, baseConfig):
             _LOGGER.warning("None for hcountBinary results, bailing")
             return
 
-        if lux.state == "unavailable" or hcountBinary.state == "unavailable":
-            _LOGGER.warning("sensors unavailable, bailing")
+        if lux.state == "unavailable":
+            _LOGGER.warning("lux sensor unavailable, bailing")
+            return
+
+        if hcountBinary.state == "unavailable":
+            _LOGGER.warning("headcount sensor unavailable, bailing")
             return
 
         # get the values we need
@@ -180,10 +168,10 @@ def setup(hass, baseConfig):
 
         if home == "on":
             nextStateValue["state"] = "present"
-            meta = _PRESENT
+            meta = self._presentDetail
         else:
             nextStateValue["state"] = "absent"
-            meta = _ABSENT
+            meta = self._absentDetail
 
         _LOGGER.debug("meta")
         _LOGGER.debug(meta)
@@ -191,9 +179,9 @@ def setup(hass, baseConfig):
         # work out minlux
         minLuxValue = meta["minLux"]
         # to stop us turning on and off at the threshold 
-        if lastKnownRunState["scene"]=="lit_scene":
-            minLuxValue=minLuxValue+_JITTER
-            _LOGGER.debug("applying jitter {} {} {}".format(meta["minLux"],_JITTER,minLuxValue))
+        if self._lastKnownRunState["scene"]=="lit_scene":
+            minLuxValue=minLuxValue+self._luxJitter
+            _LOGGER.debug("applying jitter {} {} {}".format(meta["minLux"],self._luxJitter,minLuxValue))
 
 
         # check lux
@@ -206,26 +194,26 @@ def setup(hass, baseConfig):
         _LOGGER.debug(nextStateValue)
 
         # do we need a transition - off gets checked for above
-        if lastKnownRunState["state"] != nextStateValue["state"]:
+        if self._lastKnownRunState["state"] != nextStateValue["state"]:
             _LOGGER.info("state different")
             # which transition
             if nextStateValue["scene"] in meta:
                 sceneToGo = meta[nextStateValue["scene"]]
                 _LOGGER.info("turning on %s", sceneToGo)
-                hass.services.call("scene", "turn_on", {"entity_id": sceneToGo})
+                self._hass.services.call("scene", "turn_on", {"entity_id": sceneToGo})
 
-            saveState(nextStateValue)
+            self.saveState(nextStateValue)
             return
 
         # same state - different scene?
-        if lastKnownRunState["scene"] != nextStateValue["scene"]:
+        if self._lastKnownRunState["scene"] != nextStateValue["scene"]:
             _LOGGER.info("scene different")
             if nextStateValue["scene"] in meta:
                 sceneToGo = meta[nextStateValue["scene"]]
                 _LOGGER.info("turning on %s", sceneToGo)
-                hass.services.call("scene", "turn_on", {"entity_id": sceneToGo})
+                self._hass.services.call("scene", "turn_on", {"entity_id": sceneToGo})
 
-            saveState(nextStateValue)
+            self.saveState(nextStateValue)
             return
 
         # nothing to do
@@ -233,21 +221,87 @@ def setup(hass, baseConfig):
 
         return
 
+    def saveState(self, data):
+        self._hass.data[DOMAIN][self._name] = json.dumps(data)
 
-    async def _async_sensor_changed(entity_id, old_state, new_state):
-        """Handle temperature changes."""
-        if new_state is None:
-            return
-        result = await hass.async_add_executor_job(handle_check)
+    def loadState(self):
+        return json.loads(self._hass.data[DOMAIN][self._name])
 
-    async_track_state_change(hass, _HEADCOUNT_BINARY, _async_sensor_changed)
-    async_track_state_change(hass, _LUX_SENSOR, _async_sensor_changed)
+    # only turn off if no-one present
+    def softOff(self):
+
+        home = (self._hass.states.get(self._headcountSensor).state)
+
+        if home=="on":
+            # turn on occupied
+            _LOGGER.info("off ignored")
+        else:
+            service_data = {"entity_id": _SCENE_OFF}
+            _LOGGER.info("{} to vacant".format(self._name))
+            lastKnownRunState = {"state": "off", "scene": _SCENE_OFF}
+
+            self._hass.services.call("scene", "turn_on", service_data)
+            self.saveState(lastKnownRunState)
+            # tell the boss
+            # hass.services.call(
+            #     "hangouts",
+            #     "send_message",
+            #     {
+            #         "target": [{"id": "UgyKl3VkJ2F2G_ykyb14AaABAagBjd2cDg"}],
+            #         "message": [{"text": "Lights OFF at the beach house"}],
+            #     },
+            # )
+
+
+    def hardOff(self):
+
+        service_data = {"entity_id": _SCENE_OFF}
+        _LOGGER.info("{} to vacant FORCED ".format(self._name))
+        lastKnownRunState = {"state": "off", "scene": _SCENE_OFF}
+        self._hass.services.call("scene", "turn_on", service_data)
+        self.saveState(lastKnownRunState)
+
+    def resetCheck(self):
+
+        lastKnownRunState = {"state": "check", "scene": "unknown"}
+        self.saveState(lastKnownRunState)
+
+
+
+def setup(hass, baseConfig):
+    """Set up is called when Home Assistant is loading our component."""
+
+    _LOGGER.info("luxlights setup")
+
+    config = baseConfig[DOMAIN]
+
+
+
+    # get the city one
+
+    luxInstances.append(luxLightInstance(hass,config["city"],"city"))
+
+
+    # TODO check this function
+    def handle_turnoff(call):
+        for instance in luxInstances:
+            instance.softOff()
+
+    # this gets called from an automation, it's a hard OFF
+    def handle_forceoff(call):
+        for instance in luxInstances:
+            instance.hardOff()
+
+    # reset my state, normally done sunset-hrs
+    def handle_enableCheck(call):
+        for instance in luxInstances:
+            instance.resetCheck()
+
 
     hass.services.register(DOMAIN, "enable_check", handle_enableCheck)
     hass.services.register(DOMAIN, "turn_off", handle_turnoff)
     hass.services.register(DOMAIN, "force_off", handle_forceoff)
 
-    saveState(lastKnownRunState)
 
     # Return boolean to indicate that initialization was successfully.
     return True
