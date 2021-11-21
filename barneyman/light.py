4692e502da7d6248433f51ecfe614bb88e0c7a0d
barneyman <barnaby.james.flint@gmail.com>
2021-11-21 17:59:09 +1100

    don't double delete

--- barneyman/light.py
+++ barneyman/light.py
@@ -98,9 +98,10 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
 
     if config_entry.title != BARNEYMAN_CONFIG_ENTRY:
 
-        _LOGGER.error("Old config entry - removing {}".format(config_entry.title))
-        hass.config_entries.async_remove(config_entry)
-        return False
+        if config_entry in hass.config_entries:
+            _LOGGER.error("Old config entry - removing {}".format(config_entry.title))
+            await hass.config_entries.async_remove(config_entry.entry_id)
+            return False
 
 
     async def async_update_options(hass, entry) -> None:
